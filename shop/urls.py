82bb4304049e5ac78d1716cc1f38170523e736d6
annu12340 <annujolly17@gmail.com>
2024-04-14 11:56:13 +0530

    Adding the eCommerce app

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.shop_page, name='shop'),
+    path('product/<product_id>', views.product_details, name='product-details'),
+    path('checkout', views.checkout, name='checkout'),
+    path('successful', views.successful, name='successful'),
+]
---
+++ shop/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import redirect, render
+from .models import Category, Product
+
+
+
+def shop_page(request):
+    category = Category.objects.all()
+    products = Product.objects.filter()
+    context = {
+        'category': category,
+        'products': products
+    }
+    return render(request, 'shop/dashboard.html', context)
+
+
+def product_details(request, product_id):
+    product_details = Product.objects.get(id=product_id)
+    ctg = Category.objects.get(name=product_details.category)
+    related_products = Product.objects.filter(category=ctg)
+    context = {
+        'product': product_details,
+        'related_products': related_products
+    }
+
+    return render(request, 'shop/product-details.html', context)
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def successful(request):
+    return render(request, 'succesfull.html')
+
