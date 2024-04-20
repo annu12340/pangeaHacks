5fb3ab22494c3162b31fc807d335f8105c9885b6
annu12340 <annujolly17@gmail.com>
2024-04-20 12:05:06 +0530

    Getting started with pangea API

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,19 @@
+import os
 from django.shortcuts import redirect, render
 from .models import Category, Product
+from pangea.config import PangeaConfig
+from pangea.services import Audit, UserIntel, Embargo, FileScan, DomainIntel
+from pangea.services.intel import HashType
+from pangea.tools import logger_set_pangea_config
+from pangea.utils import get_prefix, hash_sha256
+from pangea.services.vault.vault import Vault
+import pangea.exceptions as pe
+from dotenv import load_dotenv
 
+
+load_dotenv()
+config = PangeaConfig(domain=os.getenv("PANGEA_DOMAIN"))  
+audit = Audit(os.getenv("PANGEA_TOKEN"), config=config) 
 
 
 def shop_page(request):
@@ -10,6 +23,7 @@ def shop_page(request):
         'category': category,
         'products': products
     }
+    # audit.log(f"User has visited shop page")
     return render(request, 'shop/dashboard.html', context)
 
 
